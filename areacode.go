9c720ee693a506475e6156e754ce3710802788d0
John Wang <johncwang@gmail.com>
2020-09-18 03:35:19 -0700

    feat: add areacode `DefaultRegion` const

--- areacode.go
+++ areacode.go
@@ -1,8 +1,21 @@
 package gophonenumbers
 
-import "github.com/ttacon/libphonenumber"
+import (
+	"strings"
 
+	"github.com/ttacon/libphonenumber"
+)
+
+// DefaultRegion is the region to be used when none is provided.
+const DefaultRegion = "US"
+
+// ParseAreaCode will attempt to extract an areacode from a phone
+// number string.
 func ParseAreaCode(numberToParse, defaultRegion string) (string, error) {
+	defaultRegion = strings.TrimSpace(defaultRegion)
+	if len(defaultRegion) == 0 {
+		defaultRegion = DefaultRegion
+	}
 	num, err := libphonenumber.Parse(numberToParse, defaultRegion)
 	if err != nil {
 		return "", err
@@ -10,6 +23,7 @@ func ParseAreaCode(numberToParse, defaultRegion string) (string, error) {
 	return GetAreaCode(num), nil
 }
 
+// GetAreaCode will return an area code given a `libphonenumber.PhoneNumber`.
 func GetAreaCode(num *libphonenumber.PhoneNumber) string {
 	// Get the cleaned number and the length of the area code.
 	natSigNumber := libphonenumber.GetNationalSignificantNumber(num)
