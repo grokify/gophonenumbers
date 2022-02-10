46c4d726a8b164ade039d5ab77d01a1350d9c7ad
John Wang <johncwang@gmail.com>
2022-02-10 02:35:47 -0800

    feat: examples: add `areacode_distance`

---
+++ apps/areacode_distance/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/grokify/gophonenumbers"
+	geo "github.com/kellydunn/golang-geo"
+)
+
+const (
+	USNYC_AREACODE = 212
+	USNYC_LAT_GOOG = 40.6976684
+	USNYC_LON_GOOG = -74.2605588
+
+	USSFO_AREACODE = 415
+	USSFO_LAT_GOOG = 37.7578149
+	USSFO_LON_GOOG = -122.5078121
+)
+
+func GcdGoogle() {
+	p1 := geo.NewPoint(USNYC_LAT_GOOG, USNYC_LON_GOOG)
+	p2 := geo.NewPoint(USSFO_LAT_GOOG, USSFO_LON_GOOG)
+
+	dist := p1.GreatCircleDistance(p2)
+	fmt.Printf("Great circle distance NYC to SFO: %v\n", dist)
+}
+
+func main() {
+	GcdGoogle()
+
+	a2g := gophonenumbers.NewAreaCodeToGeo()
+	a2g.ReadData()
+
+	dist, err := a2g.GcdAreaCodes(USNYC_AREACODE, USSFO_AREACODE)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Great circle distance %v to %v: %v\n", USNYC_AREACODE, USSFO_AREACODE, dist)
+	fmt.Println("DONE")
+}
