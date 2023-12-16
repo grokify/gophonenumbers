ac4d69609159328c1a9f895f62dc91e1aa646e8d
John Wang <johncwang@gmail.com>
2023-12-15 20:37:26 -0800

    dep: go mod: update

--- numverify/client.go
+++ numverify/client.go
@@ -34,7 +34,7 @@ func (nc *Client) Validate(params Params) (*Response, *http.Response, error) {
 	if len(params.AccessKey) == 0 {
 		params.AccessKey = nc.AccessKey
 	}
-	apiURL, err := urlutil.URLAddQueryString(ValidateEndpoint, params.MapStringSlice())
+	apiURL, err := urlutil.URLStringAddQuery(ValidateEndpoint, params.MapStringSlice(), false)
 	if err != nil {
 		return nil, nil, err
 	}
