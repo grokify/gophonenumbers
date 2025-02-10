69ec7595dc2d8c599de0421de0792fee60bc21ab
John Wang <johncwang@gmail.com>
2025-02-10 00:31:36 -0800

    feat: `gophonenumbers`: add `Formats{}`, `FormatsParse()`, `FormatsFromPhoneNumber()`

--- e164.go
+++ e164.go
@@ -8,9 +8,7 @@ import (
 
 func E164Format(numberToParse, defaultRegion string, numberFormat phonenumbers.PhoneNumberFormat) (string, error) {
 	defaultRegion = strings.ToUpper(strings.TrimSpace(defaultRegion))
-	if len(defaultRegion) == 0 {
-		defaultRegion = "US"
-	}
+	// empty string region is used when number is already in E.164 format.
 	phone, err := phonenumbers.Parse(numberToParse, defaultRegion)
 	if err != nil {
 		return "", err
@@ -24,4 +22,28 @@ func MustE164Format(numberToParse, defaultRegion string, numberFormat phonenumbe
 		panic(err)
 	}
 	return pn
+}
+
+func FormatsParse(number, region string) (Formats, error) {
+	pn, err := phonenumbers.Parse(number, region)
+	if err != nil {
+		return Formats{}, err
+	}
+	return FormatsFromPhoneNumber(pn), nil
+}
+
+type Formats struct {
+	E164          string
+	International string
+	National      string
+	RFC3966       string
+}
+
+func FormatsFromPhoneNumber(pn *phonenumbers.PhoneNumber) Formats {
+	return Formats{
+		E164:          phonenumbers.Format(pn, phonenumbers.E164),
+		International: phonenumbers.Format(pn, phonenumbers.INTERNATIONAL),
+		National:      phonenumbers.Format(pn, phonenumbers.NATIONAL),
+		RFC3966:       phonenumbers.Format(pn, phonenumbers.RFC3966),
+	}
 }
--- e164_test.go
+++ e164_test.go
@@ -7,8 +7,8 @@ import (
 )
 
 var e164FormatTests = []struct {
-	v    string
-	want string
+	v            string
+	wantNational string
 }{
 	{"+16505550100", "(650) 555-0100"},
 }
@@ -17,11 +17,15 @@ func TestE164Format(t *testing.T) {
 	for _, tt := range e164FormatTests {
 		nat, err := E164Format(tt.v, "", phonenumbers.NATIONAL)
 		if err != nil {
-			t.Errorf("phonenumber.E164Format(\"%v\") Error: [%v]", tt.v, err.Error())
+			t.Errorf("phonenumber.E164Format(\"%s\") Error: (%s)", tt.v, err.Error())
 		}
-		if nat != tt.want {
-			t.Errorf("phonenumber.E164Format(\"%v\") Mismatch: want [%v], got [%v]",
-				tt.v, tt.want, nat)
+		if nat != tt.wantNational {
+			t.Errorf("phonenumber.E164Format(\"%v\") Mismatch: want (%s), got (%vs",
+				tt.v, tt.wantNational, nat)
+		}
+		_, err = phonenumbers.Parse(tt.v, "")
+		if err != nil {
+			t.Errorf("phonenumbers.Parse(\"%s\") Error: (%s)", tt.v, err.Error())
 		}
 	}
 }
